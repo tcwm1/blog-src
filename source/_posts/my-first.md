fde250c4e1290d745bfc1ab5b97a1e6b073b297f
Meng Wang <bibicall@bibicalldeMacBook-Pro.local>
2018-06-20 14:55:05 +0800

    update

---
+++ source/_posts/my-first.md
@@ -0,0 +1,5 @@
+---
+title: my first
+date: 2018-06-20 13:25:10
+tags:
+---
---
+++ themes/backup/update.sh
@@ -0,0 +1,2 @@
+git clone https://github.com/litten/hexo-theme-yilia.git themes/yilia
+cp themes/backup/yilia/_config.yml themes/yilia
