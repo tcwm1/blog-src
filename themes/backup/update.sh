8a46339802e722c83f1ed562ebfcd257a05dd07a
wangmeng09 <wangmeng09@baidu.com>
2018-06-20 17:01:33 +0800

    update

--- themes/backup/update.sh
+++ themes/backup/update.sh
@@ -1,2 +1,4 @@
 git clone https://github.com/litten/hexo-theme-yilia.git themes/yilia
 cp themes/backup/yilia/_config.yml themes/yilia
+cp themes/backup/favicon.ico themes/yilia/source
+cp themes/backup/CNAME themes/yilia/source
